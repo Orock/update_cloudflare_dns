35de89af5d1a0aafefc7e164ddad57b4ee0b20e0
Erick Viedma <eviemda@gmail.com>
2020-02-13 23:32:31 -0300

    add time

--- update_dns.sh
+++ update_dns.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
+CURRENT_PUBLIC_IP=$(wget ifconfig.me -q -O -)
+CURRENT_TIME=$(date +'%Y-%m-%d %H:%M:%S')
+
 if [ -z "$CLOUDFLARE_TOKEN" ]; then
-  printf "Falta la variable de entorno CLOUDFLARE_TOKEN\n"
+  printf "[${CURRENT_TIME}]Falta la variable de entorno CLOUDFLARE_TOKEN\n"
   exit 0
 fi
 
 if [ -z "$CLOUDFLARE_ZONE_ID" ]; then
-  printf "Falta la variable de entorno CLOUDFLARE_ZONE_ID\n"
+  printf "[${CURRENT_TIME}]Falta la variable de entorno CLOUDFLARE_ZONE_ID\n"
   exit 0
 fi
 
 if [ -z "$CLOUDFLARE_DNS_RECORD_ID" ]; then
-  printf "Falta la variable de entorno CLOUDFLARE_DNS_RECORD_ID\n"
+  printf "[${CURRENT_TIME}]Falta la variable de entorno CLOUDFLARE_DNS_RECORD_ID\n"
   exit 0
 fi
 
 if [ -z "$CLOUDFLARE_DOMAIN_NAME" ]; then
-  printf "Falta la variable de entorno CLOUDFLARE_DOMAIN_NAME\n"
+  printf "[${CURRENT_TIME}]Falta la variable de entorno CLOUDFLARE_DOMAIN_NAME\n"
   exit 0
 fi
-
-CURRENT_PUBLIC_IP=$(wget ifconfig.me -q -O -)
 
 FILE="current_ip"
 if test -f "$FILE"; then
@@ -30,13 +31,13 @@ else
 fi
 
 if [ "$LAST_PUBLIC_IP" = "$CURRENT_PUBLIC_IP" ]; then 
-  echo "Nada que actualizar" 
+  printf "[${CURRENT_TIME}]Nada que actualizar" 
 else
   CLOUDFLARE_ENDPOINT="https://api.cloudflare.com/client/v4/zones/${CLOUDFLARE_ZONE_ID}/dns_records/${CLOUDFLARE_DNS_RECORD_ID}"
   DATA='{"type":"A","name":"'"$CLOUDFLARE_DOMAIN_NAME"'","content":"'"$CURRENT_PUBLIC_IP"'"}'
 
-  printf "Actualizando ${CLOUDFLARE_DOMAIN_NAME}\n"
-  printf "IP Publica ${CURRENT_PUBLIC_IP}\n"
+  printf "[${CURRENT_TIME}]Actualizando ${CLOUDFLARE_DOMAIN_NAME}\n"
+  printf "[${CURRENT_TIME}]IP Publica ${CURRENT_PUBLIC_IP}\n"
 
   RESPONSE=$(curl -sX PUT "${CLOUDFLARE_ENDPOINT}" \
               -H "Authorization: ${CLOUDFLARE_TOKEN}" \
@@ -46,7 +47,7 @@ else
 
   RESULT=$(echo ${RESPONSE} | grep -Po '"success":\w*' | grep -Po 'true|false')
 
-  printf "Succes: ${RESULT}\n"
+  printf "[${CURRENT_TIME}]Succes: ${RESULT}\n"
 
   if [ "$RESULT" = "true" ]; then 
     echo "$CURRENT_PUBLIC_IP" > current_ip
